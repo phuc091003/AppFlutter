5387dd833056ef120e9bf743a1fa955c47d96d57
phuc091003 <huuphucnguyen73@gmail.com>
2025-09-07 19:48:32 +0700

    update

--- project_NHP/AppEcomerce/appecomerce/lib/main.dart
+++ project_NHP/AppEcomerce/appecomerce/lib/main.dart
@@ -1,19 +1,8 @@
-import 'package:appecomerce/utils/theme/theme.dart';
+import 'package:appecomerce/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode : ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
